98fdb460e93a86cae9760ad75af50cb7ca634782
liiana40 <60198436+liiana40@users.noreply.github.com>
2022-11-05 21:45:50 -0400

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+router.get('/', async (req, res) => {
+    let books
+    try {
+        books = Book.find().sort({ created: 'desc'}).limit(10).exec()
+    } catch {
+        books = []
+    }
+    res.render('index', { books: books })
+})
+
+module.exports = router
